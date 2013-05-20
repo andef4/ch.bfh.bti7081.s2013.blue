f274ca9b7cdc930e173a6984315bf3795c0796b2
Fabio Anderegg <andef4@bfh.ch>
2013-05-20 15:09:05 +0200

    open patient specific detail view

--- src/main/java/ch/bfh/bti7081/s2013/blue/ui/PatientDetailView.java
+++ src/main/java/ch/bfh/bti7081/s2013/blue/ui/PatientDetailView.java
@@ -1,18 +1,38 @@
 package ch.bfh.bti7081.s2013.blue.ui;
 
+import ch.bfh.bti7081.s2013.blue.entities.Patient;
+import ch.bfh.bti7081.s2013.blue.service.PatientService;
+
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
 
 @SuppressWarnings("serial")
 public class PatientDetailView extends VerticalLayout implements View, IBackButtonView {
+	private Label firstNameLabel;
+	private Label lastNameLabel;
+
+	public PatientDetailView() {
+		setSizeFull();
+		
+		FormLayout formLayout = new FormLayout();
+		firstNameLabel = new Label();
+		lastNameLabel = new Label();
+		firstNameLabel.setCaption("Vorname:");
+		lastNameLabel.setCaption("Nachname:");
+		formLayout.addComponent(firstNameLabel);
+		formLayout.addComponent(lastNameLabel);
+		addComponent(formLayout);
+	}
 	
 	@Override
 	public void enter(ViewChangeEvent event) {
-		setSizeFull();
-		Label label = new Label("Patient detail view");
-		addComponent(label);
+		Long id = Long.parseLong(event.getParameters());
+		Patient patient = PatientService.createContainer().getItem(id).getEntity();
+		firstNameLabel.setValue(patient.getFirstName());
+		lastNameLabel.setValue(patient.getLastName());
 	}
 
 	@Override
--- src/main/java/ch/bfh/bti7081/s2013/blue/ui/PatientSearchView.java
+++ src/main/java/ch/bfh/bti7081/s2013/blue/ui/PatientSearchView.java
@@ -41,7 +41,7 @@ public class PatientSearchView extends VerticalLayout implements View, IBackButt
 		table = new Table("Patients");
 		table.setContainerDataSource(patientContainer);
 		table.setVisibleColumns(new String[]{"firstName", "lastName", "birthday"});
-		table.setImmediate(true);
+		table.setSelectable(true);
 		addComponent(table);
 		
 		initButton();
@@ -71,7 +71,8 @@ public class PatientSearchView extends VerticalLayout implements View, IBackButt
 		table.addItemClickListener(new ItemClickEvent.ItemClickListener() {
 			@Override
 			public void itemClick(ItemClickEvent event) {
-				UI.getCurrent().getNavigator().navigateTo(NavigatorUI.PATIENT_DETAIL_VIEW);
+				Long id = (Long) event.getItemId();
+				UI.getCurrent().getNavigator().navigateTo(NavigatorUI.PATIENT_DETAIL_VIEW + "/" + id);
 			}
 		});
 		
