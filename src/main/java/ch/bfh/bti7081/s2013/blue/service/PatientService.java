92c5ea75937fcf68a15c17dcfcb205f57c26dfa1
Fabio Anderegg <andef4@bfh.ch>
2013-05-20 13:33:55 +0200

    moved jpa container creation to service layer

--- src/main/java/ch/bfh/bti7081/s2013/blue/service/PatientService.java
+++ src/main/java/ch/bfh/bti7081/s2013/blue/service/PatientService.java
@@ -1,6 +1,15 @@
 package ch.bfh.bti7081.s2013.blue.service;
 
-public class PatientService {
+import ch.bfh.bti7081.s2013.blue.entities.Patient;
+
+import com.vaadin.addon.jpacontainer.JPAContainer;
+import com.vaadin.addon.jpacontainer.JPAContainerFactory;
+
+public class PatientService implements IService {
+
+	public static JPAContainer<Patient> createContainer() {
+		return JPAContainerFactory.make(Patient.class, PERSISTENCE_UNIT_NAME);
+	}
 	
 
 }
