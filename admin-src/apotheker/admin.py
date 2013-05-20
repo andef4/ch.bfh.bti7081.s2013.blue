75997483acdcfcb7a2e05534593b594807a281bd
Fabio Anderegg <andef4@bfh.ch>
2013-05-20 17:58:26 +0200

    make medical drugs searchable in admin

--- admin-src/apotheker/admin.py
+++ admin-src/apotheker/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from apotheker.models import Activesubstance, Distributor, Manufacturer, Medicaldrug, MedicaldrugActivesubstance, Patient, Prescription, Prescriptionitem, Report, User
 
+class MedicalDrugAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
 admin.site.register(Activesubstance)
 admin.site.register(Distributor)
 admin.site.register(Manufacturer)
-admin.site.register(Medicaldrug)
+admin.site.register(Medicaldrug, MedicalDrugAdmin)
 admin.site.register(MedicaldrugActivesubstance)
 admin.site.register(Patient)
 admin.site.register(Prescription)
