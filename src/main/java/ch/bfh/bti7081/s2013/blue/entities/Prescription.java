db1affcf980a447222c041182a6fede3526303d9
Fabio Anderegg <andef4@bfh.ch>
2013-05-20 17:58:43 +0200

    added prescriptions to generated test data

--- src/main/java/ch/bfh/bti7081/s2013/blue/entities/Prescription.java
+++ src/main/java/ch/bfh/bti7081/s2013/blue/entities/Prescription.java
@@ -6,12 +6,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Prescription {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private long id;
+	
+	@ManyToOne
+	private Patient patient;
 	
 	private Date date;
 	private String doctor;
@@ -33,5 +37,11 @@ public class Prescription {
 	}
 	public void setDoctor(String doctor) {
 		this.doctor = doctor;
+	}
+	public Patient getPatient() {
+		return patient;
+	}
+	public void setPatient(Patient patient) {
+		this.patient = patient;
 	}
 }
--- src/main/java/ch/bfh/bti7081/s2013/blue/utils/GenerateTestData.java
+++ src/main/java/ch/bfh/bti7081/s2013/blue/utils/GenerateTestData.java
@@ -1,14 +1,21 @@
 package ch.bfh.bti7081.s2013.blue.utils;
 
 import java.sql.Date;
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
+import ch.bfh.bti7081.s2013.blue.entities.MedicalDrug;
 import ch.bfh.bti7081.s2013.blue.entities.Patient;
+import ch.bfh.bti7081.s2013.blue.entities.Prescription;
+import ch.bfh.bti7081.s2013.blue.entities.PrescriptionItem;
 
 public class GenerateTestData {
 	
@@ -19,8 +26,24 @@ public class GenerateTestData {
 	    
 	    em.getTransaction().begin();
 	    
+	    em.createQuery("DELETE FROM PrescriptionItem").executeUpdate();
+	    em.createQuery("DELETE FROM Report").executeUpdate();
+	    em.createQuery("DELETE FROM Prescription").executeUpdate();
+	    em.createQuery("DELETE FROM Patient").executeUpdate();
+	    
 	    String[] firstNames = {"Fritz", "Ueli", "Peter", "Tobias", "Marcel"};
 	    String[] lastNames = {"Berger", "Brügger", "Müller"};
+	    String[] drugs = {"Fluconax 200 mg, Kapseln", "Dafalgan, Brausetabletten 500 mg",
+	    		"Azaimun 50 mg, Tabletten", "Lyrica 150 mg, Kapseln", "Mimpara 60 mg, Filmtabletten"};
+	    String doctor = "Gregory House";
+	    Date prescriptionStart = getDate(2013, 0, 1);
+	    Date prescriptionEnd = getDate(2013, 11, 31);
+	    
+	    TypedQuery<MedicalDrug> query = em.createQuery("SELECT drug FROM MedicalDrug drug WHERE drug.name IN :drugs", MedicalDrug.class);
+	    query.setParameter("drugs", Arrays.asList(drugs));
+	    List<MedicalDrug> medicalDrugs = query.getResultList();
+	    
+	    int[] amount = {0, 0, 1, 1, 2};
 	    
 		for (String firstName : firstNames) {
 			for (String lastName : lastNames) {
@@ -29,20 +52,43 @@ public class GenerateTestData {
 				patient.setLastName(lastName);
 				
 				Random r = new Random();
-				
-				Calendar calendar = Calendar.getInstance();
-				calendar.set(Calendar.YEAR, 1930 + r.nextInt(80));
-				calendar.set(Calendar.MONTH,  + r.nextInt(12));
-				calendar.set(Calendar.DAY_OF_MONTH, 1 + r.nextInt(28));
-				Date date = new Date(calendar.getTime().getTime());
-				patient.setBirthday(date);
+				patient.setBirthday(getDate(1930 + r.nextInt(80), r.nextInt(12), 1 + r.nextInt(28)));
 				em.persist(patient);
+				
+				Prescription prescription = new Prescription();
+				prescription.setDate(prescriptionStart);
+				prescription.setDoctor(doctor);
+				prescription.setPatient(patient);
+				em.persist(prescription);
+				
+				int count = 1 + r.nextInt(4);
+				Collections.shuffle(medicalDrugs);
+				for (int i = 0; i < count; i++) {
+					PrescriptionItem item = new PrescriptionItem();
+					item.setStartDate(prescriptionStart);
+					item.setEndDate(prescriptionEnd);
+					item.setPrescription(prescription);
+					item.setMedicalDrug(medicalDrugs.get(i));
+					item.setMorning(amount[r.nextInt(5)]);
+					item.setNoon(amount[r.nextInt(5)]);
+					item.setEvening(amount[r.nextInt(5)]);
+					item.setNight(amount[r.nextInt(5)]);
+					em.persist(item);
+				}
 			}
 		}
 		em.getTransaction().commit();
 		em.close();
-
 	}
+	
+	private static Date getDate(int year, int month, int day) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.YEAR, year);
+		calendar.set(Calendar.MONTH,  month);
+		calendar.set(Calendar.DAY_OF_MONTH, day);
+		return new Date(calendar.getTime().getTime());
+	}
+	
 
 }
 
