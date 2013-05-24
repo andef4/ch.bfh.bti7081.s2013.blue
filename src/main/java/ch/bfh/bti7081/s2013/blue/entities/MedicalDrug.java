9a63652cf71ab336a2e406e774264f61a9285d1a
Fabio Anderegg <andef4@bfh.ch>
2013-05-24 15:52:24 +0200

    added hashCode and equals method to MedicalDrug Entity

--- src/main/java/ch/bfh/bti7081/s2013/blue/entities/MedicalDrug.java
+++ src/main/java/ch/bfh/bti7081/s2013/blue/entities/MedicalDrug.java
@@ -75,4 +75,25 @@ public class MedicalDrug {
 	public List<ActiveSubstance> getActiveSubstances() {
 		return activeSubstances;
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MedicalDrug other = (MedicalDrug) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
 }
