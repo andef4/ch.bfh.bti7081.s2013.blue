f6e5a89f3b977e8441e6cc127a35a81c70e5feec
kambf1 <kambf1@.bfh.ch>
2013-06-11 19:54:22 +0200

    DrugSearchView

--- src/main/java/ch/bfh/bti7081/s2013/blue/ui/DrugSearchView.java
+++ src/main/java/ch/bfh/bti7081/s2013/blue/ui/DrugSearchView.java
@@ -8,12 +8,12 @@ import com.vaadin.event.ItemClickEvent;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.Table;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
-import com.vaadin.ui.Button.ClickEvent;
 
 @SuppressWarnings("serial")
 public class DrugSearchView extends VerticalLayout implements View, IBackButtonView {
@@ -43,7 +43,7 @@ public class DrugSearchView extends VerticalLayout implements View, IBackButtonV
 		table.setContainerDataSource(drugContainer);
 		//drugContainer.addNestedContainerProperty("manufacturer.name");
 		table.setVisibleColumns(new String[]{"name","stock"});
-		table.setColumnHeader("manufacturer.name", "manufacturer");
+		//table.setColumnHeader("manufacturer.name", "manufacturer");
 		
 		table.setImmediate(true);
 		addComponent(table);
@@ -60,7 +60,7 @@ public class DrugSearchView extends VerticalLayout implements View, IBackButtonV
 				drugContainer.removeContainerFilters("name");
 				//drugContainer.removeContainerFilters("swissmedicNumber");
 				//drugContainer.removeContainerFilters("manufacturer");
-				drugContainer.removeContainerFilters("stock");
+				//drugContainer.removeContainerFilters("stock");
 				
 				String firstName = drugNameField.getValue().trim();
 				//String lastName = idNameField.getValue().trim();
@@ -78,22 +78,19 @@ public class DrugSearchView extends VerticalLayout implements View, IBackButtonV
 		table.addItemClickListener(new ItemClickEvent.ItemClickListener() {
 			@Override
 			public void itemClick(ItemClickEvent event) {
-				String name = (String) event.getItemId();
-				UI.getCurrent().getNavigator().navigateTo(NavigatorUI.DRUG_DETAIL_VIEW + "/" + name);
+				Long id = (Long) event.getItemId();
+				UI.getCurrent().getNavigator().navigateTo(NavigatorUI.DRUG_DETAIL_VIEW + "/" + id);
 			}
 		});
 		
 	}
 
-	
+	public void enter(ViewChangeEvent event) {
+	}
 
 	@Override
 	public String getBackView() {
 		return NavigatorUI.MAIN_VIEW;
-	}
-
-	@Override
-	public void enter(ViewChangeEvent event) {
 	}
 
 }
